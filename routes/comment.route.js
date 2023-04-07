6a5b5446f708e969b37cce661d204bef350d60a1
Roman Kadelnikov <artemkinkin@mail.ru>
2023-04-07 08:57:52 +0300

    Update comment.route.js

--- routes/comment.route.js
+++ routes/comment.route.js
@@ -3,8 +3,7 @@ const commentRoute = new Router();
 import { checkAuth } from '../utils/checkAuth.js';
 import { createComment } from '../controllers/comments.controller.js';
 
-// Create Comment
-// http://localhost:3002/api/comments/:id
+
 commentRoute.post('/:id', checkAuth, createComment);
 
 export default commentRoute;
